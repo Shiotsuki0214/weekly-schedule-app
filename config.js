771771363a410b83533a927ec6f0fffafc23ce75
Shiotsuki0214 <splice.lures_5i@icloud.com>
2025-06-01 19:36:13 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,21 @@
-// config.js - この内容を実際の値に変更してください
-window.APP_CONFIG = {
-    CLIENT_ID: '128812767208-k0aqkvnccauv1r7ukajqsungulquit4u.apps.googleusercontent.com',
+const CONFIG = {
+    // Google Cloud Consoleで取得したAPIキー
     API_KEY: 'AIzaSyAi6BG9NeJFHh6s565E7obnbE27wpmqP-k',
+    
+    // Google Cloud Consoleで取得したOAuth 2.0クライアントID
+    CLIENT_ID: '128812767208-k0aqkvnccauv1r7ukajqsungulquit4u.apps.googleusercontent.com'',
+    
+    // 使用するGoogle APIのスコープ
+    SCOPES: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets',
+    
+    // Google APIのディスカバリードキュメント
+    DISCOVERY_DOCS: [
+        'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+        'https://sheets.googleapis.com/$discovery/rest?version=v4'
+    ],
+    
+    // 更新対象のGoogleスプレッドシートID
+    // URLの/d/と/editの間の文字列
+    // 例: https://docs.google.com/spreadsheets/d/SPREADSHEET_ID_HERE/edit
     SPREADSHEET_ID: '1RUYuKXKIKqMZ7M6B9cJnRHEsoAGqx1tkqXgBvK8jzLk'
 };
