9b681aaa6ec159977579391e0a5a1a512fa4ad54
Shiotsuki0214 <splice.lures_5i@icloud.com>
2025-06-01 17:53:20 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js - この内容を実際の値に変更してください
+window.APP_CONFIG = {
+    CLIENT_ID: '128812767208-k0aqkvnccauv1r7ukajqsungulquit4u.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyAi6BG9NeJFHh6s565E7obnbE27wpmqP-k',
+    SPREADSHEET_ID: '1RUYuKXKIKqMZ7M6B9cJnRHEsoAGqx1tkqXgBvK8jzLk'
+};
