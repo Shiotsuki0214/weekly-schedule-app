d9eb0232779291eca1f6038d2e8fc9b06d5c02f3
Shiotsuki0214 <splice.lures_5i@icloud.com>
2025-06-07 13:54:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,15 @@ const CONFIG = {
     CLIENT_ID: '128812767208-k0aqkvnccauv1r7ukajqsungulquit4u.apps.googleusercontent.com',
     
     // 使用するGoogle APIのスコープ
-    SCOPES: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets',
+    SCOPES: 'https://www.googleapis.com/auth/calendar',
     
     // Google APIのディスカバリードキュメント
     DISCOVERY_DOCS: [
-        'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
-        'https://sheets.googleapis.com/$discovery/rest?version=v4'
+        'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
     ],
     
     // 更新対象のGoogleスプレッドシートID
     // URLの/d/と/editの間の文字列
     // 例: https://docs.google.com/spreadsheets/d/SPREADSHEET_ID_HERE/edit
-    SPREADSHEET_ID: '1RUYuKXKIKqMZ7M6B9cJnRHEsoAGqx1tkqXgBvK8jzLk'
+    // SPREADSHEET_ID: '1RUYuKXKIKqMZ7M6B9cJnRHEsoAGqx1tkqXgBvK8jzLk'
 };
